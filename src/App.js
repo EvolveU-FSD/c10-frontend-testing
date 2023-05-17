2ccb99b51a28c1a97cafc3878166e62272f05847
Danielle Barker - Mac <caseybarker86@gmail.com>
2023-05-17 11:13:16 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      const data = await response.json();
+      setTodos(data);
+    };
+    getData();
+  }, []);
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p data-testid="count">Count is {count}</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      {todos.map((todo) => {
+        return <p data-testid={"todo" + todo.id}>{todo.content}</p>;
+      })}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
-test('renders learn react link', () => {
+import App from "./App";
+
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  let count = screen.getByTestId("count");
+  expect(count).toHaveTextContent("Count is 0");
+  const button = screen.getByText("Click me");
+  act(() => {
+    button.click();
+  });
+
+  expect(count).toHaveTextContent("Count is 1");
+});
+
+test("renders item#1", async () => {
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve([{ id: 1, title: "delectus aut autem" }]),
+    })
+  );
+  render(<App />);
+
+  let text = await screen.findByTestId("todo1");
+  expect(text).toHaveTextContent("delectus aut autem");
+  expect(text).toBeInTheDocument();
 });
